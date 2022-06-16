702a3b01ae5b0bc8889f4ae4caf25eb67d8b292a
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-06-16 16:46:45 +0100

    start project

---
+++ client/src/index.ts
@@ -0,0 +1,11 @@
+﻿import http from 'http';
+import express from 'express';
+
+const app = express();
+
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 5001;
+server.listen(PORT, () => {
+    console.log('\n🚀 Server is running & up...🚀');
+});
