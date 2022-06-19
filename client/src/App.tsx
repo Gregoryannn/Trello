607e55592789d889daa916e228830463eac5651c
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-06-19 12:07:49 +0100

    added drag & drop feature

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,25 +5,28 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Board from './Pages/Board';
 import HomePage from './Pages/HomePage';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
 import PageNotFound from './Pages/PageNotFound';
 
 ReactModal.setAppElement('#root');
-
 const App = () => {
     return (
-            <div className="w-full h-screen flex flex-col">
-                <Navbar />
-                    <div
-                    className="w-full flex-1"
-                    style={{ maxHeight: `calc(100% - 2.5rem)` }}
-                >
-                    <Switch>
-                        <Route path="/boards" component={HomePage} />
-                        <Route path="/b/:boardId" component={Board} />
-                        <Route component={PageNotFound} />
-                    </Switch>
-                </div>
+        <div className="w-full h-screen flex flex-col">
+            <Navbar />
+            <div
+                className="w-full flex-1"
+                style={{ maxHeight: `calc(100% - 2.5rem)` }}
+            >
+                <Switch>
+                    <Route exact path="/" component={Login} />
+                    <Route path="/register" component={Register} />
+                    <Route path="/boards" component={HomePage} />
+                    <Route path="/b/:boardId" component={Board} />
+                    <Route component={PageNotFound} />
+                </Switch>
             </div>
-            );
+        </div>
+    );
 };
-            export default App;
+export default App;
--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -6,28 +6,15 @@ import { GrAttachment } from 'react-icons/gr';
 import { ImParagraphLeft } from 'react-icons/im';
 import { IoCloseOutline, IoPersonOutline } from 'react-icons/io5';
 import { MdDateRange } from 'react-icons/md';
+import { RiDeleteBin6Line } from 'react-icons/ri';
 import { VscChecklist } from 'react-icons/vsc';
-import ReactModal from 'react-modal';
-import { CardStatusType, itemType } from '../types';
+import { CardStatusType, itemType, DndTypes } from '../types';
+import ReactModal from './ReactModal';
 
 interface CardProps {
     item: itemType;
     cardStatus: CardStatusType;
 }
-
-const modalStyles = {
-    overlay: {
-        backgroundColor: 'rgb(0 0 0 / 75%)',
-    },
-    content: {
-        top: '50px',
-        left: '20%',
-        right: '20%',
-        bottom: '50px',
-        backgroundColor: '#f4f5f7',
-        padding: '10px',
-    },
-};
 
 const initialState: itemType = {
     _id: '',
@@ -35,7 +22,6 @@ const initialState: itemType = {
     description: '',
     status: '',
 };
-
 type ACTIONTYPE =
     | { type: 'setTitle'; value: string }
     | { type: 'setDescription'; value: string }
@@ -56,53 +42,45 @@ const inputReducer = (state: typeof initialState, action: ACTIONTYPE) => {
 
 const Card = ({ item, cardStatus }: CardProps) => {
     const descFieldRef = useRef<HTMLTextAreaElement>(null);
-
     const [modalOpen, setModalOpen] = useState(false);
     const [descEdit, setDescEdit] = useState(false);
     const [itemInput, dispatch] = useReducer(inputReducer, initialState);
-
     useEffect(() => {
         dispatch({ type: 'setItemInput', value: item });
     }, [item]);
-
     useEffect(() => {
         if (descEdit) {
             descFieldRef.current?.focus();
         }
     }, [descEdit]);
 
-    const [{ opacity }, dragRef] = useDrag(
+  // dnd hooks
+  const [{ isDragging }, dragRef] = useDrag(
         {
-            type: 'div',
-            item: { _id: item._id },
+            type: DndTypes.CARD,
+            item: { id: item._id },
             collect: (monitor) => ({
-                opacity: monitor.isDragging() ? 0.5 : 1,
+                isDragging: !!monitor.isDragging(),
             }),
-            end: () => {
-                console.log(item._id);
-            },
+          
         },
         []
     );
-
     const closeModal = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.stopPropagation();
         setModalOpen(false);
     };
-
     const titleChangedAndSave = () => {
         console.log(itemInput);
     };
-
     const descChangedAndSave = () => {
         console.log(itemInput);
     };
-
     return (
         <div
             ref={dragRef}
-            className="w-full bg-white h-12 flex-shrink-0 px-3 text-sm rounded shadow cursor-pointer"
-            style={{ opacity }}
+            className={`w-full bg-white h-12 flex-shrink-0 px-3 text-sm rounded shadow cursor-pointer ${isDragging ? 'hidden' : 'block'
+                }`}
             onClick={() => setModalOpen(true)}
             role="presentation"
         >
@@ -119,144 +97,146 @@ const Card = ({ item, cardStatus }: CardProps) => {
                 </div>
             </div>
             <p className="w-full truncate">{item?.title}</p>
-            <ReactModal
-                isOpen={modalOpen}
-                onRequestClose={closeModal}
-                contentLabel="Example Modal"
-                style={modalStyles}
-            >
-                <div className="w-full p-2 text-gray-700">
-                    <div className="w-full flex items-center space-x-4">
-                        <div className="flex-1 flex items-center space-x-3 text-xl">
-                            <div>
-                                <BiCreditCard />
-                            </div>
-                            <input
-                                type="text"
-                                value={itemInput?.title}
-                                onChange={(e) =>
-                                    dispatch({ type: 'setTitle', value: e.target.value })
-                                }
-                                onBlur={titleChangedAndSave}
-                                className="w-full bg-transparent font-bold focus:outline-none focus:bg-white rounded px-2 focus:ring-2 focus:ring-blue-500"
-                            />
-                        </div>
-                        <button
-                            type="button"
-                            onClick={closeModal}
-                            className="w-8 h-8 flex items-center justify-center bg-gray-200 font-bold text-xl hover:bg-gray-300 rounded-full cursor-pointer"
-                        >
-                            <IoCloseOutline />
-                        </button>
-                    </div>
-                    <div className="w-full flex space-x-3 mt-3">
-                        <div className="flex-1 ">
-                            <div className="w-full flex items-center space-x-3 text-lg">
+               <ReactModal modalOpen={modalOpen} closeModal={closeModal}>
+                    <div className="w-full p-2 text-gray-700">
+                        <div className="w-full flex items-center space-x-4">
+                            <div className="flex-1 flex items-center space-x-3 text-xl">
                                 <div>
-                                    <ImParagraphLeft />
+                                    <BiCreditCard />
                                 </div>
-                                <p className="bg-transparent text-sm font-semibold outline-none focus:bg-white rounded px-2 focus:ring-2 focus:ring-blue-500">
-                                    Description
-                                </p>
-                                {itemInput?.description !== '' ? (
-                                    <button
-                                        onClick={() => setDescEdit(true)}
-                                        type="button"
-                                        className="h-8 px-2 bg-gray-200 hover:bg-gray-300 text-sm rounded"
-                                    >
-                                        Edit
-                                    </button>
-                                ) : null}
-                            </div>
-                            <div className="w-full mt-3 pl-10">
-                                <textarea
-                                    ref={descFieldRef}
-                                    id="description"
-                                    name="description"
-                                    className="w-full bg-gray-200 focus:bg-white rounded p-1 px-2 outline-none focus:ring-2 focus:ring-blue-500 text-sm"
-                                    value={itemInput?.description}
-                                    onClick={() => setDescEdit(true)}
+                                <input
+                                    type="text"
+                                    value={itemInput?.title}
                                     onChange={(e) =>
-                                        dispatch({ type: 'setDescription', value: e.target.value })
+                                        dispatch({ type: 'setTitle', value: e.target.value })
                                     }
+                                    onBlur={titleChangedAndSave}
+                                    className="w-full bg-transparent font-bold focus:outline-none focus:bg-white rounded px-2 focus:ring-2 focus:ring-blue-500"
+                                />
+                            </div>
+                            <div className="flex items-center space-x-2">
+                                <button
+                                    type="button"
+                                    className="w-8 h-8 flex text-sm items-center justify-center bg-red-300 font-bold hover:bg-red-400 rounded-full cursor-pointer"
                                 >
-                                    {' '}
-                                </textarea>
-                                {descEdit && (
-                                    <div className="flex items-center mt-2 space-x-2">
-                                        <button
-                                            type="button"
-                                            className="w-12 h-8 rounded flex text-sm text-white items-center justify-center font-semibold bg-blue-500 hover:bg-blue-600 shadow"
-                                            onClick={() => {
-                                                descChangedAndSave();
-                                                setDescEdit(false);
-                                            }}
-                                        >
-                                            Save
-                                        </button>
-                                        <button
-                                            onClick={() => setDescEdit(false)}
-                                            type="button"
-                                            className="w-8 h-8 rounded flex items-center text-2xl justify-center font-bold hover:text-gray-800"
-                                        >
-                                            <IoCloseOutline />
-                                        </button>
-                                    </div>
-                                )}
+                                    <RiDeleteBin6Line />
+                                </button>
+                                <button
+                                    type="button"
+                                    onClick={closeModal}
+                                    className="w-8 h-8 flex items-center justify-center bg-gray-200 font-bold text-xl hover:bg-gray-300 rounded-full cursor-pointer"
+                                >
+                                    <IoCloseOutline />
+                                </button>
                             </div>
                         </div>
-                        <div className="w-44">
-                            <p className="text-sm uppercase mb-3">ADD TO CARD</p>
-                            <div className="w-ful flex flex-col space-y-2 text-sm">
-                                <button
-                                    type="button"
-                                    className="h-8 rounded bg-gray-200 flex items-center space-x-2 px-2 hover:bg-gray-300"
-                                >
-                                    <IoPersonOutline />
-                                    <span>Members</span>
-                                </button>
-                                <button
-                                    type="button"
-                                    className="h-8 rounded bg-gray-200 flex items-center space-x-2 px-2 hover:bg-gray-300"
-                                >
-                                    <BiLabel />
-                                    <span>Labels</span>
-                                </button>
-                                <button
-                                    type="button"
-                                    className="h-8 rounded bg-gray-200 flex items-center space-x-2 px-2 hover:bg-gray-300"
-                                >
-                                    <VscChecklist />
-                                    <span>Checklist</span>
-                                </button>
-                                <button
-                                    type="button"
-                                    className="h-8 rounded bg-gray-200 flex items-center space-x-2 px-2 hover:bg-gray-300"
-                                >
-                                    <MdDateRange />
-                                    <span>Date</span>
-                                </button>
-                                <button
-                                    type="button"
-                                    className="h-8 rounded bg-gray-200 flex items-center space-x-2 px-2 hover:bg-gray-300"
-                                >
-                                    <GrAttachment />
-                                    <span>Attachments</span>
-                                </button>
-                                <button
-                                    type="button"
-                                    className="h-8 rounded bg-gray-200 flex items-center space-x-2 px-2 hover:bg-gray-300"
-                                >
-                                    <BsCardImage />
-                                    <span>Cover</span>
-                                </button>
+                        <div className="w-full flex space-x-3 mt-3">
+                            <div className="flex-1 ">
+                                <div className="w-full flex items-center space-x-3 text-lg">
+                                    <div>
+                                        <ImParagraphLeft />
+                                    </div>
+                                    <p className="bg-transparent text-sm font-semibold outline-none focus:bg-white rounded px-2 focus:ring-2 focus:ring-blue-500">
+                                        Description
+                                    </p>
+                                    {itemInput?.description !== '' ? (
+                                        <button
+                                            onClick={() => setDescEdit(true)}
+                                            type="button"
+                                            className="h-8 px-2 bg-gray-200 hover:bg-gray-300 text-sm rounded"
+                                        >
+                                            Edit
+                                        </button>
+                                    ) : null}
+                                </div>
+                                <div className="w-full mt-3 pl-10">
+                                    <textarea
+                                        ref={descFieldRef}
+                                        id="description"
+                                        name="description"
+                                        className="w-full bg-gray-200 focus:bg-white rounded p-1 px-2 outline-none focus:ring-2 focus:ring-blue-500 text-sm"
+                                        value={itemInput?.description}
+                                        onClick={() => setDescEdit(true)}
+                                        onChange={(e) =>
+                                            dispatch({ type: 'setDescription', value: e.target.value })
+                                        }
+                                    >
+                                        {' '}
+                                    </textarea>
+                                    {descEdit && (
+                                        <div className="flex items-center mt-2 space-x-2">
+                                            <button
+                                                type="button"
+                                                className="w-12 h-8 rounded flex text-sm text-white items-center justify-center font-semibold bg-blue-500 hover:bg-blue-600 shadow"
+                                                onClick={() => {
+                                                    descChangedAndSave();
+                                                    setDescEdit(false);
+                                                }}
+                                            >
+                                                Save
+                                            </button>
+                                            <button
+                                                onClick={() => setDescEdit(false)}
+                                                type="button"
+                                                className="w-8 h-8 rounded flex items-center text-2xl justify-center font-bold hover:text-gray-800"
+                                            >
+                                                <IoCloseOutline />
+                                            </button>
+                                        </div>
+                                    )}
+                                </div>
+                            </div>
+                            <div className="w-44">
+                                <p className="text-sm uppercase mb-3">ADD TO CARD</p>
+                                <div className="w-ful flex flex-col space-y-2 text-sm">
+                                    <button
+                                        type="button"
+                                        className="h-8 rounded bg-gray-200 flex items-center space-x-2 px-2 hover:bg-gray-300"
+                                    >
+                                        <IoPersonOutline />
+                                        <span>Members</span>
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="h-8 rounded bg-gray-200 flex items-center space-x-2 px-2 hover:bg-gray-300"
+                                    >
+                                        <BiLabel />
+                                        <span>Labels</span>
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="h-8 rounded bg-gray-200 flex items-center space-x-2 px-2 hover:bg-gray-300"
+                                    >
+                                        <VscChecklist />
+                                        <span>Checklist</span>
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="h-8 rounded bg-gray-200 flex items-center space-x-2 px-2 hover:bg-gray-300"
+                                    >
+                                        <MdDateRange />
+                                        <span>Date</span>
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="h-8 rounded bg-gray-200 flex items-center space-x-2 px-2 hover:bg-gray-300"
+                                    >
+                                        <GrAttachment />
+                                        <span>Attachments</span>
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="h-8 rounded bg-gray-200 flex items-center space-x-2 px-2 hover:bg-gray-300"
+                                    >
+                                        <BsCardImage />
+                                        <span>Cover</span>
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </ReactModal>
+                </ReactModal>
         </div>
     );
 };
-
 export default Card;
